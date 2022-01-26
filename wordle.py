0ace338795c0da3df41cd39db1a8c997c87d29db
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-01-26 13:45:01 +0000

    Make better guesses for the first word

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 import os
+import string
 from typing import Dict, List, Set, Tuple
 
 def get_initial_words() -> Dict[str, int]:
@@ -45,8 +46,39 @@ def filter_words(
 
     return out
 
-def get_best_word(possible_words: Dict[str, int]) -> str:
+def get_letters_by_rank(words: Dict[str, int]) -> List[str]:
+    letter_tallies: Dict[str, int] = {}
 
+    for letter in string.ascii_lowercase:
+        letter_tallies[letter] = 0
+
+    for word in words:
+        for char in word:
+            letter_tallies[char.lower()] += 1
+
+    return [item[0] for item in sorted(letter_tallies.items(), key=lambda x: x[1])]
+
+def get_best_first_word(possible_words: Dict[str, int]) -> str:
+    """Ranks by letter frequency, as opposed to word frequency, for an optimum first guess"""
+    letters_by_rank = get_letters_by_rank(possible_words)
+
+    best_word = ""
+    best_word_score = 0
+
+    for word in possible_words:
+        score = 0
+        already_scored_letters: Set[str] = set()
+        for character in word:
+            if character not in already_scored_letters:
+                score += letters_by_rank.index(character)
+                already_scored_letters.add(character)
+
+        if score > best_word_score:
+            best_word = word
+            best_word_score = score
+    return best_word
+
+def get_best_word(possible_words: Dict[str, int]) -> str:
     best_word = ""
     best_word_score = 0
 
@@ -72,8 +104,9 @@ def play():
     fixed_letters: List[Tuple[int, str]] = []
     fixed_not_letters: List[Tuple[int, str]] = []
 
+    current_guess: str = get_best_first_word(current_words)
+
     while len(fixed_letters) < 5:
-        current_guess = get_best_word(current_words)
         print(f"\nTry '{current_guess}'\n")
 
         res = get_input(f"Was {current_guess} valid? (y/n): ", {"y", "n"})
@@ -102,6 +135,7 @@ def play():
             raise Exception("Enter y, or n")
 
         current_words = filter_words(known_letters, known_not_letters, known_not_words, fixed_letters, fixed_not_letters, current_words)
+        current_guess = get_best_word(current_words)
 
     print("\nSUCCESS!!")
 
